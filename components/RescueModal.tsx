14bec2e51ea6cfd2a291c7ccb79c032d55c09d8a
Tina Zheng <tina.s.zheng+github@gmail.com>
2023-01-26 23:13:34 -0500

    token balances

--- components/RescueModal.tsx
+++ components/RescueModal.tsx
@@ -1,4 +1,6 @@
 import styled from '@emotion/styled'
+import { BigNumber } from 'ethers'
+import { parseEther } from 'ethers/lib/utils.js'
 import { useState } from 'react'
 import Modal from 'react-modal'
 import { RescueInput } from './RescueInput'
@@ -16,16 +18,41 @@ const customStyles = {
   },
 }
 
+function prepareTokenBalances(balances: any) {
+  const output: { [address: string]: BigNumber } = {}
+  for (var i = 0; i < balances.tokens.length; i++) {
+    output[balances[i].token.address as string] = parseEther(String(balances[i].quantity))
+  }
+
+  return output
+}
+
 export function RescueModal({
   showModal,
   setShowModal,
   confirmRescue,
+  recipientAddress,
+  tokenBalances,
 }: {
   showModal: boolean
   setShowModal: (arg0: boolean) => void
   confirmRescue: (arg0: any) => void
+  recipientAddress: string
+  tokenBalances: any
 }) {
   const [approveAddress, setApproveAddress] = useState('')
+  const [error, setError] = useState<string | null>(null)
+  const [loading, setLoading] = useState(false)
+
+  const onConfirm = async () => {
+    if (approveAddress.toLowerCase() !== recipientAddress.toLowerCase()) {
+      setError('Wrong recipient address!')
+      return
+    }
+
+    const balances = prepareTokenBalances(tokenBalances)
+    // sign data here
+  }
 
   return (
     <Modal
@@ -40,18 +67,29 @@ export function RescueModal({
       <div style={{ fontSize: '20px', paddingTop: '24px', paddingBottom: '24px' }}>
         Verify with the owner that this is the correct address
       </div>
-      <div style={{ display: 'flex', fontSize: '32px' }}>
-        <div style={{ flex: '1', color: '#888FAB', cursor: 'pointer' }} onClick={() => setShowModal(false)}>
+      {error && <div style={{ color: 'red' }}>{error}</div>}
+      <ModalButtons>
+        <button style={{ color: '#888FAB', cursor: 'pointer' }} onClick={() => setShowModal(false)}>
           Cancel
-        </div>
-        <div style={{ flex: '2' }} />
-        <div style={{ flex: '1', textAlign: 'right', color: '#4C82FB', cursor: 'pointer' }} onClick={confirmRescue}>
-          Confirm
-        </div>
-      </div>
+        </button>
+        <button style={{ color: '#4C82FB', cursor: 'pointer' }} onClick={confirmRescue}>
+          {loading ? 'Sign in wallet...' : 'Confirm'}
+        </button>
+      </ModalButtons>
     </Modal>
   )
 }
+
+const ModalButtons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  > button {
+    background: none;
+    border: none;
+    padding: 0;
+    font-size: 32px;
+  }
+`
 
 const Container = styled.div`
   position: absolute;
