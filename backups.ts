15ae5156c3591907abdcf901e25b40f49f2624de
Mark Toda <toda.mark@gmail.com>
2023-01-26 22:54:49 -0800

    feat: add new contract

--- backups.ts
+++ backups.ts
@@ -29,7 +29,7 @@ const PAL_SIGNATURE_TYPES = {
 
 const PERMIT2_ADDRESS = '0x000000000022d473030f116ddee9f6b43ac78ba3'
 // TODO: this is a placeholder address
-export const TOKEN_BACKUPS_ADDRESS = '0x76cDA3E5F2ff00067db42428d9AB79Ae1977411F'
+export const TOKEN_BACKUPS_ADDRESS = '0xc6dd15D910E7F464c7dC714A774754DA69606514'
 // nonce doesnt really matter, just has to be unique 256 bits
 export const BACKUP_NONCE = BigNumber.from('6969696969696969696969696969696969696969420')
 
