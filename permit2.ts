72533fc28282b726221bcd4a3fc94f697e98fd9e
Mark Toda <toda.mark@gmail.com>
2023-04-19 00:43:08 -0700

    feat: add get permit data

--- permit2.ts
+++ permit2.ts
@@ -1,10 +1,12 @@
 import { BigNumber, ethers } from 'ethers';
 import { Contract as MulticallContract, Provider as MulticallProvider } from 'ethers-multicall';
 import Permit2Abi from '@/abis/Permit2.json'
-import { TypedDataDomain, TypedDataField } from '@ethersproject/abstract-signer';
+import { Permit2__factory } from '@/types/ethers-contracts';
 import {
   PermitBatch,
+  PermitBatchData,
   AllowanceTransfer,
+  MaxSigDeadline
 } from '@uniswap/permit2-sdk'
 
 const PERMIT2="0x000000000022d473030f116ddee9f6b43ac78ba3"
@@ -42,4 +44,43 @@ export async function useAllowances(provider: ethers.providers.Provider, owner: 
     allowances[token][spender] = { amount, expiration };
     return allowances;
   });
+}
+
+export async function useAllowances(provider: ethers.providers.Provider, owner: string, tokenSpenders: TokenSpender[]): Promise<Allowances> {
+  const multicallProvider = new MulticallProvider(provider);
+  await multicallProvider.init();
+  const permit2Contract = new MulticallContract(PERMIT2, Permit2Abi);
+
+  const calls = tokenSpenders.map(({ token, spender }) =>
+    permit2Contract.allowance(owner, token, spender)
+  );
+
+  const results = await multicallProvider.all(calls);
+
+  return results.reduce((allowances, [amount, expiration], i) => {
+    const { token, spender } = tokenSpenders[i];
+    if (!allowances[token]) {
+      allowances[token] = {};
+    }
+    allowances[token][spender] = { amount, expiration };
+    return allowances;
+  });
+}
+
+export function buildRevokePermit(chainId: number, nonce: BigNumber, spender: string, tokens: string[]): PermitBatchData {
+  const permitBatch: PermitBatch = {
+    details: tokens.map((token) => ({
+      token,
+      // no more allowance anymore
+      amount: 0,
+      // also it instantly expires
+      expiration: 0,
+      nonce,
+    })),
+    spender,
+    sigDeadline: MaxSigDeadline,
+  };
+
+  // dump this response into wallet.signTypedData
+  return AllowanceTransfer.getPermitData(permitBatch, PERMIT2, chainId) as PermitBatchData;
 }
