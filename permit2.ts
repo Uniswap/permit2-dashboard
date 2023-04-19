c59c3e2999d057b382e3b2d7c3df7841e3fa7b11
Mark Toda <toda.mark@gmail.com>
2023-04-19 00:46:47 -0700

    feat: add revoke handler

--- permit2.ts
+++ permit2.ts
@@ -1,4 +1,4 @@
-import { BigNumber, ethers } from 'ethers';
+import { ethers, BigNumber, ContractTransaction } from 'ethers';
 import { Contract as MulticallContract, Provider as MulticallProvider } from 'ethers-multicall';
 import Permit2Abi from '@/abis/Permit2.json'
 import { Permit2__factory } from '@/types/ethers-contracts';
@@ -46,41 +46,7 @@ export async function useAllowances(provider: ethers.providers.Provider, owner: 
   });
 }
 
-export async function useAllowances(provider: ethers.providers.Provider, owner: string, tokenSpenders: TokenSpender[]): Promise<Allowances> {
-  const multicallProvider = new MulticallProvider(provider);
-  await multicallProvider.init();
-  const permit2Contract = new MulticallContract(PERMIT2, Permit2Abi);
-
-  const calls = tokenSpenders.map(({ token, spender }) =>
-    permit2Contract.allowance(owner, token, spender)
-  );
-
-  const results = await multicallProvider.all(calls);
-
-  return results.reduce((allowances, [amount, expiration], i) => {
-    const { token, spender } = tokenSpenders[i];
-    if (!allowances[token]) {
-      allowances[token] = {};
-    }
-    allowances[token][spender] = { amount, expiration };
-    return allowances;
-  });
-}
-
-export function buildRevokePermit(chainId: number, nonce: BigNumber, spender: string, tokens: string[]): PermitBatchData {
-  const permitBatch: PermitBatch = {
-    details: tokens.map((token) => ({
-      token,
-      // no more allowance anymore
-      amount: 0,
-      // also it instantly expires
-      expiration: 0,
-      nonce,
-    })),
-    spender,
-    sigDeadline: MaxSigDeadline,
-  };
-
-  // dump this response into wallet.signTypedData
-  return AllowanceTransfer.getPermitData(permitBatch, PERMIT2, chainId) as PermitBatchData;
+export async function revoke(owner: ethers.Wallet, toRevoke: TokenSpender[]): Promise<ContractTransaction> {
+  const permit2Contract = Permit2__factory.connect(PERMIT2, owner);
+  return await permit2Contract.lockdown(toRevoke);
 }
