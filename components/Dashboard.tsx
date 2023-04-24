db0dcd0e1c037129555e73184b88db946f799dae
marktoda <toda.mark@gmail.com>
2023-04-24 16:55:22 -0700

    feat: add revoke button (#4)

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,12 +1,14 @@
 import { useAsyncData } from '@/util/hooks'
-import { getAllowancesForAddress } from '@/permit2'
+import { revoke } from '@/permit2'
 import { useCallback } from 'react'
-import { useProvider, useAccount } from 'wagmi'
+import { useProvider, useAccount, useSigner } from 'wagmi'
+import { getAllowancesForAddress } from '@/permit2'
 import moment from 'moment';
 
 export default function Dashboard() {
   const provider = useProvider()
   const { address } = useAccount()
+  const { data: signer } = useSigner()
 
   const fetchData = useCallback(() => {
     if (!address) return
@@ -14,10 +16,17 @@ export default function Dashboard() {
     return getAllowancesForAddress(provider, address)
   }, [provider, address])
 
-
   const { data: spendersAndTokens } = useAsyncData(fetchData)
 
   if (!spendersAndTokens) return <div>Loading...</div>
+
+  const onRevoke = async (spender: string, tokens: string[]) => {
+    if (!signer) {
+      console.log('no spender')
+      return
+    }
+    await revoke(signer, tokens.map((token) => ({ spender, token })))
+  }
 
   return <div>{Object.keys(spendersAndTokens)?.map((spender) => (
     <div key={spender}>
@@ -27,6 +36,12 @@ export default function Dashboard() {
           <div>Token: {token}</div>
           <div>amount: {amount.toString()}</div>
           <div>expiration: {moment(expiration * 1000).fromNow()}</div>
+
+          {signer && amount.gt(0) && expiration > Date.now() &&
+            <button onClick={() => onRevoke(spender, [token])}>
+              Revoke
+            </button>
+          }
         </div>
       ))}
     </div>
